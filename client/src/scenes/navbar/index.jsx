85e4f5ad2ed34ceaf20b21a9b807c0429e530fe9
Almedin <almedin@almedin.domain.name>
2022-12-31 16:48:08 +0100

    First commit

---
+++ client/src/scenes/navbar/index.jsx
@@ -0,0 +1,38 @@
+import {
+  Box,
+  IconButton,
+  Typography,
+  InputBase,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "../../state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../../components/FlexBetween";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispath = useDispatch();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme();
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
